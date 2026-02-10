e205528755ee53522a9e76f60f9412d053bc33ac
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-10 19:37:55 +0100

    finished function ConvertToNodeList

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Melih7342/huffman-file-compression
+
+go 1.25.6
--- huffman.go
+++ huffman.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"slices"
+	"sort"
+
+	"github.com/Melih7342/huffman-file-compression/models"
+)
+
 func FrequencyCounter(bytes []byte) map[byte]int {
 	frequencies := make(map[byte]int)
 	for i := 0; i < len(bytes); i++ {
@@ -12,8 +19,24 @@ func FrequencyCounter(bytes []byte) map[byte]int {
 	return frequencies
 }
 
+func ConvertToNodeList(m map[byte]int) []*models.Node {
+	nodes := make([]*models.Node, 0, len(m))
+	for value, frequency := range m {
+		newNode := &models.Node{
+			Value:     value,
+			Frequency: frequency}
+		nodes = append(nodes, newNode)
+	}
+	slices.SortFunc(nodes, func(a, b *models.Node) int {
+		return a.Frequency - b.Frequency
+	})
+	return nodes
+}
+
 func Huffman(bytes []byte) []byte {
 	// Check byte frequencies
 	frequencies := FrequencyCounter(bytes)
 
+	// Convert the Map of character-values and frequencies to a list of nodes
+	ConvertToNodeList(frequencies)
 }
