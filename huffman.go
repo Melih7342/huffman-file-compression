f0f0a0d291f677ed5ecd6e527892712dce7366f8
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-10 19:11:14 +0100

    created struct Node

--- huffman.go
+++ huffman.go
@@ -1,1 +1,19 @@
 package main
+
+func FrequencyCounter(bytes []byte) map[byte]int {
+	frequencies := make(map[byte]int)
+	for i := 0; i < len(bytes); i++ {
+		if _, ok := frequencies[bytes[i]]; ok {
+			frequencies[bytes[i]]++
+		} else {
+			frequencies[bytes[i]] = 1
+		}
+	}
+	return frequencies
+}
+
+func Huffman(bytes []byte) []byte {
+	// Check byte frequencies
+	frequencies := FrequencyCounter(bytes)
+
+}
