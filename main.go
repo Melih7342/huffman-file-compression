0341068b44789d6aa8c893913761505b0c832312
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-12 20:53:45 +0100

    added -h flag for a help/manual output

--- main.go
+++ main.go
@@ -12,19 +12,29 @@ import (
 
 func main() {
 	// Define flags
-	compressMode := flag.Bool("c", false, "Compress files")
-	decompressMode := flag.Bool("d", false, "DecompressFile files")
-	verbosity := flag.Bool("v", false, "Verbosity")
+	compressMode := flag.Bool("c", false, "Compress file(s)")
+	decompressMode := flag.Bool("d", false, "Decompress file(s)")
+	verbosity := flag.Bool("v", false, "Verbose output")
 	directory := flag.Bool("r", false, "Recursive directory content compression")
-	// help := flag.Bool("h", false, "Help")
-	outputPath := flag.String("o", "", "Output file path")
+	help := flag.Bool("h", false, "Help")
+	outputPath := flag.String("o", "", "Choose custom output location")
 
 	// Read files from command line
 	flag.Parse()
+
+	if *help {
+		flag.Usage()
+	}
+
 	initialPaths := flag.Args()
 
+	// console output for -h or when no files are provided
 	if len(initialPaths) == 0 {
-		fmt.Println("Usage: huffman -c/-d file1 file2 ...")
+		fmt.Println("\nExamples:")
+		fmt.Println(" - Compression: ./huff -c test.txt")
+		fmt.Println(" - Decompression: ./huff -d test.txt.huff")
+		fmt.Println(" - Define output location: ./huff -c -o C:/Users/myUser test.txt")
+		fmt.Println(" - Compress files in a directory: ./huff -c -r C:/Users/myUser")
 		return
 	}
 
