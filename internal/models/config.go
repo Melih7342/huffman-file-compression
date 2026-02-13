e24b149c43e0c74e5d35bbb0d6ac99b73c3b2238
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-13 11:36:59 +0100

    relocated flag configuration to separate file for cleaner main.go

---
+++ internal/models/config.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	CompressMode   bool
+	DecompressMode bool
+	Verbosity      bool
+	Recursive      bool
+	Performance    bool
+	OutputPath     string
+	InputPaths     []string
+}
+
+func ParseConfig() *Config {
+	c := flag.Bool("c", false, "Compress file(s)")
+	d := flag.Bool("d", false, "Decompress file(s)")
+	v := flag.Bool("v", false, "Verbose output")
+	r := flag.Bool("r", false, "Recursive directory content compression")
+	h := flag.Bool("h", false, "Help")
+	p := flag.Bool("p", false, "Performance metrics")
+	o := flag.String("o", "", "Choose custom output location")
+
+	flag.Parse()
+
+	if *h {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		printExamples()
+		os.Exit(0)
+	}
+
+	return &Config{
+		CompressMode:   *c,
+		DecompressMode: *d,
+		Verbosity:      *v,
+		Recursive:      *r,
+		Performance:    *p,
+		OutputPath:     *o,
+		InputPaths:     args,
+	}
+}
+
+func printExamples() {
+	fmt.Println("\nExamples:")
+	fmt.Println(" - Compression: ./huff -c test.txt")
+	fmt.Println(" - Decompression: ./huff -d test.txt.huff")
+	fmt.Println(" - Define output location: ./huff -c -o C:/Users/myUser test.txt")
+	fmt.Println(" - Compress files in a directory: ./huff -c -r C:/Users/myUser")
+}
