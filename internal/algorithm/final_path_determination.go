a0a6d95a68511efba0ce6dce60d58c232c13ee86
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-14 21:35:16 +0100

    refactoring and tests for final path determination

--- internal/algorithm/final_path_determination.go
+++ internal/algorithm/final_path_determination.go
@@ -9,20 +9,36 @@ import (
 	"github.com/Melih7342/huffman-file-compression/internal/models"
 )
 
-func DetermineFinalPath(source string, cfg models.Config, fileCount int) string {
+func DetermineFinalPath(source string, cfg models.Config, fileCount int) (string, error) {
+	sourceInfo, err := os.Stat(source)
+	if err != nil {
+		return "", fmt.Errorf("could not stat source: %w", err)
+	}
+	if sourceInfo.IsDir() {
+		return "", fmt.Errorf("please provide a filepath")
+	}
+
 	var finalPath string
 
 	if cfg.OutputPath != "" {
-		info, err := os.Stat(cfg.OutputPath)
-		isDir := err == nil && info.IsDir()
+		outputInfo, err := os.Stat(cfg.OutputPath)
+		outputIsDir := err == nil && outputInfo.IsDir()
 
-		if fileCount > 1 || isDir {
+		if fileCount > 1 || outputIsDir {
 			fileName := filepath.Base(source)
 
 			if cfg.CompressMode {
-				fileName += ".huff"
+				if filepath.Ext(fileName) != ".huff" {
+					fileName += ".huff"
+				} else {
+					return "", fmt.Errorf("the file %s has extension .huff and likely already compressed", fileName)
+				}
 			} else if cfg.DecompressMode {
-				fileName = strings.TrimSuffix(fileName, ".huff")
+				if filepath.Ext(fileName) == ".huff" {
+					fileName = strings.TrimSuffix(fileName, ".huff")
+				} else {
+					return "", fmt.Errorf("the file %s doesn't have extension .huff and likely isn't compressed", fileName)
+				}
 			}
 
 			finalPath = filepath.Join(cfg.OutputPath, fileName)
@@ -36,10 +52,18 @@ func DetermineFinalPath(source string, cfg models.Config, fileCount int) string 
 		}
 	} else {
 		if cfg.CompressMode {
-			finalPath = source + ".huff"
+			if filepath.Ext(source) != ".huff" {
+				source += ".huff"
+			} else {
+				return "", fmt.Errorf("the file %s has extension .huff and likely already compressed", source)
+			}
 		} else if cfg.DecompressMode {
-			finalPath = strings.TrimSuffix(source, ".huff")
+			if filepath.Ext(source) == ".huff" {
+				source = strings.TrimSuffix(source, ".huff")
+			} else {
+				return "", fmt.Errorf("the file %s doesn't have extension .huff and likely isn't compressed", source)
+			}
 		}
 	}
-	return finalPath
+	return finalPath, nil
 }
---
+++ internal/algorithm/final_path_determination_test.go
@@ -0,0 +1,86 @@
+package algorithm
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/Melih7342/huffman-file-compression/internal/models"
+)
+
+func TestDetermineFinalPath(t *testing.T) {
+	tempDir := t.TempDir()
+
+	file1 := filepath.Join(tempDir, "file1.txt")
+	file2 := filepath.Join(tempDir, "file2.txt.huff")
+
+	os.WriteFile(file1, []byte("file1"), 0644)
+	os.WriteFile(file2, []byte("file2"), 0644)
+
+	fileCount := 1
+
+	test := []struct {
+		name           string
+		input          string
+		cfg            models.Config
+		expectedOutput string
+		expectError    bool
+	}{
+		{
+			name:  "Compress mode with standard .txt extension",
+			input: file1,
+			cfg: models.Config{
+				CompressMode: true,
+				OutputPath:   tempDir,
+			},
+			expectedOutput: "file1.txt.huff",
+		},
+		{
+			name:  "Compress mode with .huff extension (already compressed)",
+			input: file2,
+			cfg: models.Config{
+				CompressMode: true,
+				OutputPath:   tempDir,
+			},
+			expectedOutput: "",
+			expectError:    true,
+		},
+		{
+			name:  "Decompress mode with .huff extension",
+			input: file2,
+			cfg: models.Config{
+				DecompressMode: true,
+				OutputPath:     tempDir,
+			},
+			expectedOutput: "file2.txt",
+		},
+		{
+			name:        "Provide a directory as source path",
+			input:       tempDir,
+			cfg:         models.Config{},
+			expectError: true,
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := DetermineFinalPath(tt.input, tt.cfg, fileCount)
+
+			if err != nil && !tt.expectError {
+				t.Fatalf("unexpected error for %s: %v", tt.name, err)
+			}
+
+			if err == nil && tt.expectError {
+				t.Fatalf("expected error for %s, but got none", tt.name)
+			}
+
+			if tt.expectError {
+				return
+			}
+
+			resultFileName := filepath.Base(result)
+			if resultFileName != tt.expectedOutput {
+				t.Errorf("%s: expecting filename %s, got %s", tt.name, tt.expectedOutput, resultFileName)
+			}
+		})
+	}
+}
